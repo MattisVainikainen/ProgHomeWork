2e1aefe792f8b287293f2af6c7f784e05b7d6699
Mattis Vainikainen <mattis.vainikainen@gmail.com>
2019-10-02 11:07:43 +0200

    update

--- Övningar/Arrays/Ovning74.cs
+++ Övningar/Arrays/Ovning74.cs
@@ -10,14 +10,18 @@ namespace ProgHomeWork.Övningar.Arrays
     {
         public void Print()
         {
+            //Array utav INTS
             int[] olikaTal = { 4, 5, 88, 3, 8};
+            // Här skapar vi en variabel MAX som använder sig utav .MAX() metoden.
             int MAX = olikaTal.Max();
+            // Här skapar vi en variabel MIN som använder sig utav .MIN() metoden.
             int MIN = olikaTal.Min();
             
+            //Här får vi reda på index platsen för MAX och MIN
             int max_position = Array.IndexOf(olikaTal, MAX);
             int min_position = Array.IndexOf(olikaTal, MIN);
 
-
+            // Vi deklarerar en variabel för varje gång den loopas.
             int antalLoops = 0;
             foreach (var number in olikaTal)
             {
