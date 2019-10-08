74c3983059705affa45dfa3e53df198da74d8af2
Mattis Vainikainen <mattis.vainikainen@gmail.com>
2019-10-08 13:54:30 +0200

    update

---
+++ GetSetExamples1/CarsToRent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetExamples1
+{
+    class CarsToRent
+    {
+        public string carName;
+        private string carColor;
+        private int carPrice;
+
+        public void RentCar()
+        {
+
+        }
+
+        public void ReturnCar()
+        {
+
+        }
+
+        public void ListOfCars(string _carName, string _carColor, int _carPrice)
+        {
+            carName = _carName;
+            carColor = _carColor;
+            carPrice = _carPrice;
+        }
+
+        public int CarPrice
+        {
+            get { return carPrice; }
+            set
+            {
+                carPrice = value;
+            }
+        }
+    }
+}
